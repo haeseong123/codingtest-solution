9bc3c0bebc768de439ca2803fb6e37c27c616a81
hs <50406129+haeseong123@users.noreply.github.com>
2023-06-09 15:42:27 +0900

    [level 2] Title: 피로도, Time: 0.47 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/87946. 피로도/피로도.py
+++ 프로그래머스/lv2/87946. 피로도/피로도.py
@@ -1,23 +1,71 @@
-answer = 0
-N = 0
-visited = []
+# <문제 설명>
+# XX게임에는 피로도 시스템(0 이상의 정수로 표현합니다)이 있으며, 
+# 일정 피로도를 사용해서 던전을 탐험할 수 있습니다. 
+# 이때, 각 던전마다 탐험을 시작하기 위해 필요한 "최소 필요 피로도"와 던전 탐험을 마쳤을 때 소모되는 "소모 피로도"가 있습니다. 
+# "최소 필요 피로도"는 해당 던전을 탐험하기 위해 가지고 있어야 하는 최소한의 피로도를 나타내며, 
+# "소모 피로도"는 던전을 탐험한 후 소모되는 피로도를 나타냅니다. 
+# 예를 들어 "최소 필요 피로도"가 80, "소모 피로도"가 20인 던전을 탐험하기 위해서는 
+# 유저의 현재 남은 피로도는 80 이상 이어야 하며, 던전을 탐험한 후에는 피로도 20이 소모됩니다.
 
+#이 게임에는 하루에 한 번씩 탐험할 수 있는 던전이 여러개 있는데, 
+# 한 유저가 오늘 이 던전들을 최대한 많이 탐험하려 합니다. 
+# 유저의 현재 피로도 k와 각 던전별 "최소 필요 피로도", "소모 피로도"가 담긴 2차원 배열 dungeons 가 매개변수로 주어질 때, 
+# 유저가 탐험할수 있는 최대 던전 수를 return 하도록 solution 함수를 완성해주세요.
 
-def dfs(k, cnt, dungeons):
-    global answer
-    if cnt > answer:
-        answer = cnt
+# -> 최대한 던전을 많이돌면 몇개 돌수 있는지?
 
-    for j in range(N):
-        if k >= dungeons[j][0] and not visited[j]:
-            visited[j] = 1
-            dfs(k - dungeons[j][1], cnt + 1, dungeons)
-            visited[j] = 0
+# <제한사항>
+# k는 1 이상 5,000 이하인 자연수입니다.
+# dungeons의 세로(행) 길이(즉, 던전의 개수)는 1 이상 8 이하입니다.
+# dungeons의 가로(열) 길이는 2 입니다.
+# dungeons의 각 행은 각 던전의 ["최소 필요 피로도", "소모 피로도"] 입니다.
+# "최소 필요 피로도"는 항상 "소모 피로도"보다 크거나 같습니다.
+# "최소 필요 피로도"와 "소모 피로도"는 1 이상 1,000 이하인 자연수입니다.
+# 서로 다른 던전의 ["최소 필요 피로도", "소모 피로도"]가 서로 같을 수 있습니다.
 
+# <문제 풀이>
+# dungeons의 개수가 얼마 되지 않으니 permutaions로 모든 조합의 배열을 구하고
+# 해당 배열을 순회하며 각각 몇개의 던전을 돌 수 있는지 cnt한뒤, cnt의 최댓값을 반환하면 된다.
+
+# 1. dungeons를 조합하는 permutations()를 작성한다.
+# 2. for i in range(1, len(dungeons)):
+#       permutation(0, i)
+# 3. 2의 결과로 얻은 배열을 순회하며 get_cnt(e)를 호출한다.
+#       결과로 얻은 cnt를 _max와 비교한다.
+# 4. _max를 반환한다.
+
+# 필요한 것
+#   -> permutations()
+#       -> 조합 함수를 작성하면 된다.
+#   -> get_cnt()
+#       -> while 
+
+def permutations(start, n, r, res):
+    if start == r:
+        res.append(n[:r])
+        return
+
+    for i in range(start, len(n)):
+        n[start], n[i] = n[i], n[start]
+        permutations(start + 1, n, r, res)
+        n[start], n[i] = n[i], n[start]
+
+def get_cnt(k, dungeons):
+    cnt = 0
+    for required, consumed in dungeons:
+        if k < required:
+            break
+        cnt += 1
+        k -= consumed
+    return cnt
 
 def solution(k, dungeons):
-    global N, visited
-    N = len(dungeons)
-    visited = [0] * N
-    dfs(k, 0, dungeons)
+    answer = 0
+    ps = []
+    for i in range(1, len(dungeons) + 1):
+        permutations(0, dungeons, i, ps)
+    
+    for p in ps:
+        answer = max(answer, get_cnt(k, p))
+    
     return answer
