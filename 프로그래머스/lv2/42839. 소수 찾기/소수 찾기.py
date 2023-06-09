b40af2a0b364ae531156265d1e93d0292ac51430
hs <50406129+haeseong123@users.noreply.github.com>
2023-06-09 14:36:43 +0900

    [level 2] Title: 소수 찾기, Time: 0.46 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.py
@@ -1,52 +1,48 @@
-# 1. `answer` 변수를 생성하고 0을 넣습니다.
-# 2. 주어진 `numbers`를 리스트로 변환합니다.
-# 3. nPr에서 모든 r에대한 순열을 생성하도록 1부터 len(numbers)까지 반복합니다.
-# 4. i를 넣어 `permutations(numbers, i)`를 호출합니다.
-# 5. 생성된 순열의 요소가 소수인지 확인합니다. 만약 소수라면 `answer += 1`을 합니다.
-# 6. `answer`를 반환합니다.
+# <문제 설명>
+# 한자리 숫자가 적힌 종이 조각이 흩어져있습니다. 
+# 흩어진 종이 조각을 붙여 소수를 몇 개 만들 수 있는지 알아내려 합니다.
 
+# 각 종이 조각에 적힌 숫자가 적힌 문자열 numbers가 주어졌을 때, 
+# 종이 조각으로 만들 수 있는 소수가 몇 개인지 return 하도록 solution 함수를 완성해주세요.
 
-# 소수를 판별하는 함수입니다.
-def is_prime(n):
-    if n < 2:
+# <제한사항>
+# numbers는 길이 1 이상 7 이하인 문자열입니다.
+# numbers는 0~9까지 숫자만으로 이루어져 있습니다.
+# "013"은 0, 1, 3 숫자가 적힌 종이 조각이 흩어져있다는 의미입니다.
+
+# <문제 풀이>
+# 순열(Permutation)을 사용하여 주어진 numbers로 만들 수 있는 모든 정수를 갖는 배열을 구합니다.
+# 배열을 순회하며 해당 정수가 소수인지 판별하고 소수가 맞다면 answer에 +1을 합니다.
+def is_prime(num):
+    if num < 2:
         return False
-    for i in range(2, int(n ** 0.5) + 1):
-        if n % i == 0:
+    
+    for i in range(2, int(num**0.5)+1):
+        if num % i == 0:
             return False
     return True
 
-
-# 순열을 생성하는 함수입니다.
-def permutations_recursion(arr, r):
-    result = set()  # 중복을 피하기 위해 set을 사용합니다.
-    n = len(arr)
-
-    def backtrack(start):
-        if start == r:
-            # 맨 앞자리가 0이면 숫자로 바꿀 때 자릿수가 
-            # 줄어드므로 result에 추가하지 않습니다.
-            if arr[0] != '0':
-                result.add(int(''.join(arr[:r])))
-            return
-        for i in range(start, n):
-            arr[start], arr[i] = arr[i], arr[start]  # 자리 바꾸기
-            backtrack(start + 1)
-            arr[start], arr[i] = arr[i], arr[start]  # 자리 원위치
-
-    backtrack(0)
-    return result
-
-
 def solution(numbers):
-    answer = 0
     numbers = list(numbers)
-
-    # nPr에서 모든 r에대한 순열을 생성하도록 1부터 len(numbers)까지 반복합니다.
+    answer = 0
+    ps = set()
+    
+    def permutations(start, r):
+        if start == r:
+            ps.add(int("".join(numbers[:r])))
+            return
+        
+        for i in range(start, len(numbers)):
+            numbers[start], numbers[i] = numbers[i], numbers[start]
+            permutations(start + 1, r)
+            numbers[start], numbers[i] = numbers[i], numbers[start]
+    
     for i in range(1, len(numbers) + 1):
-        # 순열을 생성합니다. P(numbers, 1), P(numbers, 2), ..., P(numbers, n)
-        for p in permutations_recursion(numbers, i):
-            # 생성된 순열이 소수인지 판별합니다.
-            if is_prime(p):
-                answer += 1
-
+        permutations(0, i)
+    
+    for p in ps:
+        if is_prime(p):
+            answer += 1
+    
     return answer
+    
