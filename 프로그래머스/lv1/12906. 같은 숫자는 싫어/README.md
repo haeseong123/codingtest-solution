2c5ef34df42e7ba4624434ca42bd95c164e5e079
hs <50406129+haeseong123@users.noreply.github.com>
2023-08-18 22:08:26 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 60.51 ms, Memory: 32.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/README.md
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/README.md
@@ -1,10 +1,10 @@
 # [level 1] 같은 숫자는 싫어 - 12906 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12906) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12906?language=python3) 
 
 ### 성능 요약
 
-메모리: 83.5 MB, 시간: 12.77 ms
+메모리: 32.4 MB, 시간: 60.51 ms
 
 ### 구분
 
--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -1,22 +1,6 @@
-# <문제 설명>
-# 배열 arr이 주어집니다. 배열 arr의 각 원소는 숫자 0~9로 이루어져있습니다.
-# 이때, 배열 arr에서 연속적으로 나타나는 숫자는 하나만 남기고 전부 제거하려 합니다.
-# 단, 제거된 후 남은 수들을 반환할 때는 배열 arr의 원소들의 순서를 유지해야 합니다. 예를 들면,
-# arr = [1, 1, 3, 3, 0, 1, 1] 이면 [1, 3, 0, 1] 을 return 합니다.
-# arr = [4, 4, 4, 3, 3] 이면 [4, 3] 을 return 합니다.
-
-# <제한사항>
-# 배열 arr의 크기 : 1,000,000 이하의 자연수
-# 배열 arr의 원소의 크기 : 0보다 크거나 같고 9보다 작거나 같은 정수
-
-# <풀이>
-# stack으로 풀면 될 것 같다.
-# 1. arr을 순회하며 아래를 반복한다.
-#   1-1. stack의 가장 위쪽에 있는 원소와 현재 순회 원소의 값이 서로 다르면 추가한다.
-# 2. stack을 반환한다.
 def solution(arr):
-    stack = [arr[0]]
-    for e in arr:
-        if stack[-1] != e:
-            stack.append(e)
-    return stack
+    answer = [arr[0]]
+    for e in arr[1:]:
+        if answer[-1] != e:
+            answer.append(e)
+    return answer
