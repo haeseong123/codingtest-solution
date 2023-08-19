978219e1a4fb4eb8bffa7006d87eb0f9aee25339
hs <50406129+haeseong123@users.noreply.github.com>
2023-08-19 20:52:22 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ 프로그래머스/lv3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -1,27 +1,40 @@
-# 현재 실행할 수 있는 작업 중 끝나는 시간이 제일 빠른 것 먼저 뽑아서
-# 실행하고, 해당 작업이 요청 된 시점부터 종료된 시간까지의 길이를 계산하여 누적하고
-# 해당 누적값 // len(jobs)를 반환하자.
-
+from collections import deque
 import heapq
 
 def solution(jobs):
+    # 지금 시점에 시작할 수 있는 작업이 있다면:
+    #   지금 시점에 시작할 수 있는 작업 중 가장 빨리 끝나는 것 수행
+    # else:
+    #   가장 빨리 시작할 수 있는 작업을 수행
+
+    # job = [도착시간, 소요시간]
     length = len(jobs)
-    acc, now = 0, 0
-    heap = []
+    jobs = deque(sorted(jobs, key=lambda x: (x[0], x[1])))
+    heap, time, answer = [], 0, 0
     
     while jobs:
-        for i, j in enumerate(jobs):
-            if j[0] <= now:
-                heapq.heappush(heap, [j[1], j[0], i])
+        # 수행 가능한 작업을 heap에 넣는다.
+        while jobs and jobs[0][0] <= time:
+            s, d = jobs.popleft()
+            heapq.heappush(heap, (d, s))
         
         if heap:
-            # 소요 시간, 도착 시간
-            need, arrival, i = heapq.heappop(heap)
-            now += need
-            acc += now - arrival
-            jobs.pop(i)
+            # 수행할 수 있는 작업이 있다면
+            # 소요시간이 가장 짧은 것을 수행
+            d, s = heapq.heappop(heap)
+            answer += (time - s) + d
+            time += d
         else:
-            now += 1
-        heap = []
+            # 수행할 수 있는 작업이 없다면
+            # 가장 빨리 수행할 수 있는 작업을 수행
+            if jobs:
+                s, d = jobs.popleft()
+                answer += d
+                time = s+d
     
-    return acc // length    
+    while heap:
+        d, s = heapq.heappop(heap)
+        answer += (time - s) + d
+        time += d
+        
+    return answer // length
