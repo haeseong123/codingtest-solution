de6f9dd35d53addf0827dab07e862a0d08187449
hs <50406129+haeseong123@users.noreply.github.com>
2023-08-19 21:26:54 +0900

    [level 2] Title: 가장 큰 수, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,22 +1,13 @@
 from functools import cmp_to_key
 
-def comparator_by_asc(a, b):
-    if a == 0:
-        return 1
-    # if b == 0:
-    #     return 0
-    
+def comparator(a, b):
     a, b = str(a), str(b)
-    c = int(a + b)
-    d = int(b + a)
+    c, d = int(a + b), int(b + a)
     if c > d:
         return -1
-    elif c == d:
+    if c == d:
         return 0
-    else:
-        return 1
+    return 1
 
 def solution(numbers):
-    sorted_numbers = sorted(numbers, key=cmp_to_key(comparator_by_asc))
-    answer = "".join(map(str, sorted_numbers)) 
-    return answer if answer[0] != "0" else "0"
+    return str(int("".join(list(map(str, sorted(numbers, key=cmp_to_key(comparator)))))))
