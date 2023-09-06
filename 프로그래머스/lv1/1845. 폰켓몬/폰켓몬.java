ccab8eb65cd4c932977bd263dde56e4cb7ad612d
hs <50406129+haeseong123@users.noreply.github.com>
2023-09-06 13:59:10 +0900

    [level 1] Title: 폰켓몬, Time: 5.43 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+
+class Solution {
+    public int solution(int[] nums) {
+        Set<Integer> s = new HashSet<>();
+        for (int num : nums) s.add(num);
+        return Math.min(s.size(), nums.length >> 1);
+    }
+}
