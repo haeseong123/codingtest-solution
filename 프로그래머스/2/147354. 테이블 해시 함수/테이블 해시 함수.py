fe53e065d23a4822b84b1bca5afcc8106cd77e8f
hs <50406129+haeseong123@users.noreply.github.com>
2023-11-09 17:00:17 +0900

    [level 2] Title: 테이블 해시 함수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.py
+++ 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.py
@@ -1,10 +1,9 @@
 def solution(data, col, row_begin, row_end):
     answer = 0
     sorted_data = sorted(data, key=lambda x: (x[col - 1], -x[0]))
-    
+
     for (i, row) in enumerate(sorted_data[row_begin - 1 : row_end]):
         row_num = i + row_begin
         answer ^= sum(n % row_num for n in row)
-    
+
     return answer
-    
